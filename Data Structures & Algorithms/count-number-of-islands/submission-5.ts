3457f54a0af390493fb4f96718eb4cd5ead80998
Robert <11r66o77b99@gmail.com>
2026-06-23 20:05:02 +0400

    Add: count-number-of-islands - submission-5

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-5.ts
@@ -0,0 +1,32 @@
+class Solution {
+    numIslands(grid: string[][]): number {
+        let islands: number = 0
+
+        for (let i = 0; i < grid.length; i++) {
+            for (let j = 0; j < grid[i].length; j++) {
+                if (+grid[i][j]) { // if it's land visit the whole island and mark as visited
+                    ++islands
+                    this.visit(grid, i, j) // pre-order dept-first search to find all connected land cells
+                }
+            }
+        }
+
+        return islands;
+    }
+
+    private visit(grid: string[][], i: number, j: number) {
+        if (i >= grid.length
+            || i < 0
+            || j >= grid[i].length
+            || j < 0) return // if out of boundaries return
+
+        if (grid[i][j] === '0') return // if it's water return
+
+        grid[i][j] = '0' // mark as visited
+
+        this.visit(grid, i, j + 1) // visit right
+        this.visit(grid, i, j - 1) // visit left
+        this.visit(grid, i + 1, j) // visit down
+        this.visit(grid, i - 1, j) // visit up
+    }
+}
