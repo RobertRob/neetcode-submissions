2039deef9b505fb239e44684108dea1ab8f57583
Robert <11r66o77b99@gmail.com>
2026-06-23 20:02:15 +0400

    Add: count-number-of-islands - submission-2

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-2.ts
@@ -0,0 +1,31 @@
+class Solution {
+    numIslands(grid: string[][]): number {
+        let islands: number = 0
+
+        for (let i = 0; i < grid.length; i++) {
+            for (let j = 0; j < grid[i].length; j++) {
+                if (+grid[i][j]) {
+                    ++islands
+                    this.visit(grid, i, j) // dept-first search
+                }
+            }
+        }
+
+        return islands;
+    }
+
+    private visit(grid: string[][], i: number, j: number) {
+        if (i >= grid.length
+            || i < 0
+            || j >= grid[i].length
+            || j < 0) return
+
+        if (grid[i][j] === '0') return
+        grid[i][j] = '0'
+
+        this.visit(grid, i, j + 1)
+        this.visit(grid, i, j - 1)
+        this.visit(grid, i + 1, j)
+        this.visit(grid, i - 1, j)
+    }
+}
